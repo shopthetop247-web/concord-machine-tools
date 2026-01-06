c2408ff117afb254d0d167cafea03bd3f5d9d28d
Jason <jktek1@gmail.com>
2026-01-06 10:14:27 -0500

    Add Tailwind configuration

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        brandBlue: "#5795f2",
+      },
+    },
+  },
+  plugins: [],
+};
+
+
