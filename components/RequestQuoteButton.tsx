b677dc40e485b84c902fe78d2867108ad105232b
shopthetop247-web <shopthetop247@gmail.com>
2026-01-06 11:15:36 -0500

    Update RequestQuoteButton.tsx

--- components/RequestQuoteButton.tsx
+++ components/RequestQuoteButton.tsx
@@ -9,7 +9,23 @@ export default function RequestQuoteButton({ stockNumber }: RequestQuoteButtonPr
   return (
     <a
       href={`mailto:sales@concordmt.com?subject=Request for Stock# ${stockNumber}`}
-      className="inline-block bg-brandBlue text-white font-semibold px-6 py-3 mt-4 rounded hover:bg-blue-400 transition-colors duration-300"
+      className="
+        inline-block 
+        bg-brandBlue 
+        text-white 
+        font-semibold 
+        px-6 py-3 
+        mt-4 
+        rounded 
+        shadow-md 
+        hover:bg-blue-400 
+        hover:shadow-lg 
+        transform 
+        hover:scale-105 
+        transition 
+        duration-300 
+        ease-in-out
+      "
     >
       Request Quote
     </a>
