54f8b0297c4e28c22ff917fc5412ec1d0e9acc71
shopthetop247-web <shopthetop247@gmail.com>
2026-03-06 09:47:28 -0500

    Update sanityClient.ts

--- lib/sanityClient.ts
+++ lib/sanityClient.ts
@@ -3,7 +3,22 @@ import { createClient } from 'next-sanity';
 
 export const client = createClient({
   projectId: 'iwodjd3n', // your Sanity project ID
-  dataset: 'production',  // your dataset
-  useCdn: false,          // set to true if you want cached data
-  apiVersion: '2023-12-31', // today's date or later
+  dataset: 'production', // your dataset
+  useCdn: false,         // set to true if you want cached data
+  apiVersion: '2023-12-31',
 });
+
+
+// RECENT MACHINES QUERY (for homepage)
+export const recentMachinesQuery = `
+*[_type == "machine"] | order(_createdAt desc)[0...4]{
+  _id,
+  title,
+  slug,
+  mainImage{
+    asset->{
+      url
+    }
+  }
+}
+`;
